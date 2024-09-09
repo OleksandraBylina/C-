a917124328037adef1e7df412963c5f07fedd677
soto <bylina.alexa2005@gmail.com>
2024-09-09 09:54:47 +0300

    Some changes

--- Homework2/bylina_2_08.cpp
+++ Homework2/bylina_2_08.cpp
@@ -11,14 +11,17 @@ int main() {
     float ha = square / (2.0 * a);
     float hb = square / (2.0 * b);
     float hc = square / (2.0 * c);
+    printf("%f %f %f \n", ha, hb, hc);
 
     float ma = 0.5 * sqrt(2.0 * pow(b, 2) + 2.0 * pow(c, 2) - pow(a, 2));
     float mb = 0.5 * sqrt(2.0 * pow(a, 2) + 2.0 * pow(c, 2) - pow(b, 2));
     float mc = 0.5 * sqrt(2.0 * pow(b, 2) + 2.0 * pow(a, 2) - pow(c, 2));
+    printf("%f %f %f \n", ma, mb, mc);
 
     float ba = sqrt(b * c * (1.0 - ((pow(a, 2) / pow((b + c), 2)))));
     float bb = sqrt(a * c * (1.0 - ((pow(b, 2) / pow((a + c), 2)))));
     float bc = sqrt(b * a * (1.0 - ((pow(c, 2) / pow((b + a), 2)))));
+    printf("%f %f %f \n", ba, bb, bc);
 
 
 }
